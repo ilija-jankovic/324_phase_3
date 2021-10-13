c83dbb708dbb60c6a83a84f7a38d7ebccd450bda
Nicole Bowkett <ntb7@students.waikato.ac.nz>
2021-10-14 12:21:32 +1300

    created recipe book form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,185 @@ namespace _324_phase_3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelRecipes = new System.Windows.Forms.Panel();
+            this.labelThaiGreenCurry = new System.Windows.Forms.Label();
+            this.pictureBoxThaiGreenCurry = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCrazyTacos = new System.Windows.Forms.PictureBox();
+            this.labelCrazyTacos = new System.Windows.Forms.Label();
+            this.pictureBoxStuffedSausages = new System.Windows.Forms.PictureBox();
+            this.labelStuffedSausages = new System.Windows.Forms.Label();
+            this.labelSpacer = new System.Windows.Forms.Label();
+            this.buttonRecipeBook = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panelRecipes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThaiGreenCurry)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCrazyTacos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStuffedSausages)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelRecipes
+            // 
+            this.panelRecipes.AutoScroll = true;
+            this.panelRecipes.BackColor = System.Drawing.Color.Transparent;
+            this.panelRecipes.Controls.Add(this.labelSpacer);
+            this.panelRecipes.Controls.Add(this.pictureBoxStuffedSausages);
+            this.panelRecipes.Controls.Add(this.labelStuffedSausages);
+            this.panelRecipes.Controls.Add(this.pictureBoxCrazyTacos);
+            this.panelRecipes.Controls.Add(this.labelCrazyTacos);
+            this.panelRecipes.Controls.Add(this.pictureBoxThaiGreenCurry);
+            this.panelRecipes.Controls.Add(this.labelThaiGreenCurry);
+            this.panelRecipes.Location = new System.Drawing.Point(0, 0);
+            this.panelRecipes.Name = "panelRecipes";
+            this.panelRecipes.Size = new System.Drawing.Size(714, 843);
+            this.panelRecipes.TabIndex = 0;
+            // 
+            // labelThaiGreenCurry
+            // 
+            this.labelThaiGreenCurry.AutoSize = true;
+            this.labelThaiGreenCurry.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelThaiGreenCurry.Font = new System.Drawing.Font("Trebuchet MS", 36F);
+            this.labelThaiGreenCurry.Location = new System.Drawing.Point(16, 245);
+            this.labelThaiGreenCurry.MinimumSize = new System.Drawing.Size(668, 70);
+            this.labelThaiGreenCurry.Name = "labelThaiGreenCurry";
+            this.labelThaiGreenCurry.Size = new System.Drawing.Size(668, 70);
+            this.labelThaiGreenCurry.TabIndex = 0;
+            this.labelThaiGreenCurry.Text = "Thai Green Curry";
+            // 
+            // pictureBoxThaiGreenCurry
+            // 
+            this.pictureBoxThaiGreenCurry.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxThaiGreenCurry.Image = global::_324_phase_3.Properties.Resources.thaiGreenCurry;
+            this.pictureBoxThaiGreenCurry.Location = new System.Drawing.Point(16, 16);
+            this.pictureBoxThaiGreenCurry.Name = "pictureBoxThaiGreenCurry";
+            this.pictureBoxThaiGreenCurry.Size = new System.Drawing.Size(668, 230);
+            this.pictureBoxThaiGreenCurry.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxThaiGreenCurry.TabIndex = 1;
+            this.pictureBoxThaiGreenCurry.TabStop = false;
+            // 
+            // pictureBoxCrazyTacos
+            // 
+            this.pictureBoxCrazyTacos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxCrazyTacos.Image = global::_324_phase_3.Properties.Resources.crazyTacos;
+            this.pictureBoxCrazyTacos.Location = new System.Drawing.Point(16, 335);
+            this.pictureBoxCrazyTacos.Name = "pictureBoxCrazyTacos";
+            this.pictureBoxCrazyTacos.Size = new System.Drawing.Size(668, 230);
+            this.pictureBoxCrazyTacos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxCrazyTacos.TabIndex = 3;
+            this.pictureBoxCrazyTacos.TabStop = false;
+            // 
+            // labelCrazyTacos
+            // 
+            this.labelCrazyTacos.AutoSize = true;
+            this.labelCrazyTacos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelCrazyTacos.Font = new System.Drawing.Font("Trebuchet MS", 36F);
+            this.labelCrazyTacos.Location = new System.Drawing.Point(16, 564);
+            this.labelCrazyTacos.MinimumSize = new System.Drawing.Size(668, 70);
+            this.labelCrazyTacos.Name = "labelCrazyTacos";
+            this.labelCrazyTacos.Size = new System.Drawing.Size(668, 70);
+            this.labelCrazyTacos.TabIndex = 2;
+            this.labelCrazyTacos.Text = "Crazy Tacos";
+            // 
+            // pictureBoxStuffedSausages
+            // 
+            this.pictureBoxStuffedSausages.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxStuffedSausages.Image = global::_324_phase_3.Properties.Resources.stuffedSausages;
+            this.pictureBoxStuffedSausages.Location = new System.Drawing.Point(16, 652);
+            this.pictureBoxStuffedSausages.Name = "pictureBoxStuffedSausages";
+            this.pictureBoxStuffedSausages.Size = new System.Drawing.Size(668, 230);
+            this.pictureBoxStuffedSausages.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxStuffedSausages.TabIndex = 5;
+            this.pictureBoxStuffedSausages.TabStop = false;
+            // 
+            // labelStuffedSausages
+            // 
+            this.labelStuffedSausages.AutoSize = true;
+            this.labelStuffedSausages.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelStuffedSausages.Font = new System.Drawing.Font("Trebuchet MS", 36F);
+            this.labelStuffedSausages.Location = new System.Drawing.Point(16, 881);
+            this.labelStuffedSausages.MinimumSize = new System.Drawing.Size(668, 70);
+            this.labelStuffedSausages.Name = "labelStuffedSausages";
+            this.labelStuffedSausages.Size = new System.Drawing.Size(668, 70);
+            this.labelStuffedSausages.TabIndex = 4;
+            this.labelStuffedSausages.Text = "Stuffed Sausages";
+            // 
+            // labelSpacer
+            // 
+            this.labelSpacer.AutoSize = true;
+            this.labelSpacer.Location = new System.Drawing.Point(338, 1108);
+            this.labelSpacer.Name = "labelSpacer";
+            this.labelSpacer.Size = new System.Drawing.Size(0, 13);
+            this.labelSpacer.TabIndex = 6;
+            // 
+            // buttonRecipeBook
+            // 
+            this.buttonRecipeBook.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.buttonRecipeBook.BackgroundImage = global::_324_phase_3.Properties.Resources.recipeBook;
+            this.buttonRecipeBook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonRecipeBook.Location = new System.Drawing.Point(0, 845);
+            this.buttonRecipeBook.Name = "buttonRecipeBook";
+            this.buttonRecipeBook.Size = new System.Drawing.Size(357, 85);
+            this.buttonRecipeBook.TabIndex = 1;
+            this.buttonRecipeBook.UseVisualStyleBackColor = false;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.buttonBrowse.BackgroundImage = global::_324_phase_3.Properties.Resources.browse;
+            this.buttonBrowse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonBrowse.Location = new System.Drawing.Point(357, 845);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(357, 85);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox3.Image = global::_324_phase_3.Properties.Resources.addRecipe;
+            this.pictureBox3.Location = new System.Drawing.Point(30, 720);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(102, 102);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(716, 931);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonRecipeBook);
+            this.Controls.Add(this.panelRecipes);
+            this.Name = "Form1";
+            this.Text = "My Recipe Book";
+            this.panelRecipes.ResumeLayout(false);
+            this.panelRecipes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThaiGreenCurry)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCrazyTacos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStuffedSausages)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelRecipes;
+        private System.Windows.Forms.PictureBox pictureBoxThaiGreenCurry;
+        private System.Windows.Forms.Label labelThaiGreenCurry;
+        private System.Windows.Forms.Label labelSpacer;
+        private System.Windows.Forms.PictureBox pictureBoxStuffedSausages;
+        private System.Windows.Forms.Label labelStuffedSausages;
+        private System.Windows.Forms.PictureBox pictureBoxCrazyTacos;
+        private System.Windows.Forms.Label labelCrazyTacos;
+        private System.Windows.Forms.Button buttonRecipeBook;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
